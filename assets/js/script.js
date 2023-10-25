e22e9c58151bd1d9368bacb8937897bc0675491f
Rachel Cutler <rach_cutler@live.co.uk>
2023-10-25 17:36:53 +0000

    linked up buttons with js and image map

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,6 +98,7 @@ class Calculator {
                 break
             case '%':
                 computation = (prev / 100) * current;
+                break
             // sqrt
             case '√':
                 break
@@ -182,43 +183,124 @@ class Calculator {
     }
 }
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
+
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+// Operator buttons
+// const multiplyButton = document.querySelector('[data-multiply]');
+// const additionButton = document.querySelector('[data-add]');
+// const subtractButton = document.querySelector('[data-subtract]');
+// const divideButton = document.querySelector('[data-divide]');
+
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
-// new buttons
+
+// Advanced operator buttons
 const percentageButton = document.querySelector('[data-percentage]');
 const piButton = document.querySelector('[data-pi]');
 const sqrtButton = document.querySelector('[data-sqrt]');
 const pow2Button = document.querySelector('[data-pow-two]');
 const pow3Button = document.querySelector('[data-pow-three]');
 const toggleButton = document.querySelector('[data-toggle-sign]');
-
 const sinButton = document.querySelector('[data-sin]');
 const cosButton = document.querySelector('[data-cos]');
 const tanButton = document.querySelector('[data-tan]');
 
 
+// Number buttons
+// const num1 = document.querySelector('[data-1]');
+// const num2 = document.querySelector('[data-2]');
+// const num3 = document.querySelector('[data-3]');
+// const num4 = document.querySelector('[data-4]');
+// const num5 = document.querySelector('[data-5]');
+// const num6 = document.querySelector('[data-6]');
+// const num7 = document.querySelector('[data-7]');
+// const num8 = document.querySelector('[data-8]');
+// const num9 = document.querySelector('[data-9]');
+// const num0 = document.querySelector('[data-0]');
+
+// num1.value = "1";
+// num2.value = "2";
+// num3.value = "3";
+// num4.value = "4";
+// num5.value = "5";
+// num6.value = "6";
+// num7.value = "7";
+// num8.value = "8";
+// num9.value = "9";
+// num0.value = "0";
+
+
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
+// -------number buttons event listeners---------------- //
+// num1.addEventListener('click', () => {
+//     calculator.appendNumber(num1.value);
+//     calculator.updateDisplay();
+// });
+// num2.addEventListener('click', () => {
+//     calculator.appendNumber(num2.value);
+//     calculator.updateDisplay();
+// });
+// num3.addEventListener('click', () => {
+//     calculator.appendNumber(num3.value);
+//     calculator.updateDisplay();
+// });
+// num4.addEventListener('click', () => {
+//     calculator.appendNumber(num4.value);
+//     calculator.updateDisplay();
+// });
+// num5.addEventListener('click', () => {
+//     calculator.appendNumber(num5.value);
+//     calculator.updateDisplay();
+// });
+// num6.addEventListener('click', () => {
+//     calculator.appendNumber(num6.value);
+//     calculator.updateDisplay();
+// });
+// num7.addEventListener('click', () => {
+//     calculator.appendNumber(num7.value);
+//     calculator.updateDisplay();
+// });
+// num8.addEventListener('click', () => {
+//     calculator.appendNumber(num8.value);
+//     calculator.updateDisplay();
+// });
+// num9.addEventListener('click', () => {
+//     calculator.appendNumber(num9.value);
+//     calculator.updateDisplay();
+// });
+// num0.addEventListener('click', () => {
+//     calculator.appendNumber(num0.value);
+//     calculator.updateDisplay();
+// });
+
+const numberElements = document.querySelectorAll('[data-number]');
+
+numberElements.forEach(element => {
+    element.addEventListener('click', () => {
+        calculator.appendNumber(element.getAttribute('data-number'));
         calculator.updateDisplay();
     })
 })
 
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText);
+const operationElements = document.querySelectorAll('[data-operation]');
+
+operationElements.forEach(element => {
+    element.addEventListener('click', () => {
+        calculator.chooseOperation(element.getAttribute('data-operation'));
         calculator.updateDisplay();
     })
 })
 
-equalsButton.addEventListener('click', button => {
+
+// operator buttons //
+
+
+equalsButton.addEventListener('click', () => {
     calculator.compute();
     calculator.updateDisplay();
 })
@@ -228,12 +310,12 @@ allClearButton.addEventListener('click', button => {
     calculator.updateDisplay();
 })
 
-deleteButton.addEventListener('click', button => {
+deleteButton.addEventListener('click', () => {
     calculator.delete();
     calculator.updateDisplay();
 })
 
-// new buttons
+
 
 percentageButton.addEventListener('click', () => {
     calculator.chooseOperation('%');
@@ -313,3 +395,4 @@ memoryRecallButton.addEventListener('click', () => {
         calculator.updateDisplay();
     }
 });
+
